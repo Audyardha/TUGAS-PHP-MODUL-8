bcdd426d7575dbee3287b743df7f5c28bd769e73
Aulyardha <aulyardhaanindita@gmail.com>
2022-12-21 20:06:51 +0700

    audy

---
+++ latihan1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h2></h2>
+    <?php
+	$inputan = 3;
+	echo("$inputan adalah ");
+	if($inputan % 4 >= 1)
+	{
+		echo("bilangan ganjil");
+	}else
+	{
+		echo("bilangan genap");
+	}
+    ?>
+</body>
+</html>
